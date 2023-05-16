4c7112c721dc8323190d8c3548405e8a93f108c4
samuel-melo1 <samuelprogramacao3@gmail.com>
2023-05-15 23:43:55 -0300

    página home

---
+++ src/main/java/com/Login/Security/controller/HomeController.java
@@ -0,0 +1,17 @@
+package com.Login.Security.controller;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+  @GetMapping("/home")
+  public String home() {
+    return "home";
+  }
+
+  @GetMapping("/")
+  public String homePage() {
+    return "redirect:home";
+  }
+}
--- src/main/java/com/Login/Security/controller/LoginController.java
+++ src/main/java/com/Login/Security/controller/LoginController.java
@@ -2,15 +2,36 @@ package com.Login.Security.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.servlet.ModelAndView;
+import com.Login.Security.model.Usuario;
+import com.Login.Security.service.UsuarioService;
+import jakarta.servlet.http.HttpServletRequest;
 
 
 @Controller
 public class LoginController {
 
+    private UsuarioService usuarioService;
+    
+    public LoginController(UsuarioService usuarioService){
+        this.usuarioService = usuarioService;
+    }
+
   @GetMapping("/login")
-  public String login() {
-    return "login";
+  public ModelAndView login() {
+    ModelAndView mv = new ModelAndView("login");
+    return mv;
   }
 
-  
+  @PostMapping("/login")
+  public ModelAndView logar(@ModelAttribute("usuario") Usuario usuario,
+          HttpServletRequest request) {
+      usuario.setEmail(request.getParameter("email"));
+      usuario.setSenha(request.getParameter("senha"));
+      Usuario usuarioSalvo = usuarioService.autenticar(usuario.getEmail(), usuario.getSenha());
+      ModelAndView mv = new ModelAndView("redirect:/home");
+      return mv;
+  }
 }
