dea76201c6ccbebed75713e7871e64d0367948a8
samuel-melo1 <samuelprogramacao3@gmail.com>
2023-05-18 23:42:23 -0300

    erro Bcrypt

--- src/main/java/com/Login/Security/configs/SecurityConfig.java
+++ src/main/java/com/Login/Security/configs/SecurityConfig.java
@@ -2,8 +2,18 @@ package com.Login.Security.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.SecurityBuilder;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.Authentication;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -13,10 +23,17 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import com.Login.Security.service.CustomUserDetailsService;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+
+  private CustomUserDetailsService userDetailsService;
+
+  public SecurityConfig(CustomUserDetailsService userDetailsService) {
+    this.userDetailsService = userDetailsService;
+  }
 
   @Bean
   public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
@@ -35,18 +52,28 @@ public class SecurityConfig {
   }
 
   @Bean
+  public AuthenticationManager authenticationManager() throws Exception {
+    return authentication -> {
+      UserDetails userDetails = userDetailsService.loadUserByUsername(authentication.getName());
+      if (userDetails != null
+          && passwordEncoder().matches(authentication.getCredentials().toString(), userDetails.getPassword())) {
+        return new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+      }
+      throw new BadCredentialsException("Usuário inexistente ou senha inválida");
+    };
+  }
+
+  @Bean
   public UserDetailsService userDetailsService() {
+    PasswordEncoder passwordEncoder = new BCryptPasswordEncoder(12);
+
     UserDetails user = User.builder()
-        .username("user")
-        .password(passwordEncoder().encode("password"))
+        .username("email")
+        .password(passwordEncoder.encode("senha"))
         .roles("USER")
         .build();
-    UserDetails admin = User.builder()
-        .username("admin")
-        .password(passwordEncoder().encode("password"))
-        .roles("USER", "ADMIN")
-        .build();
-    return new InMemoryUserDetailsManager(user, admin);
+
+    return new InMemoryUserDetailsManager(user);
   }
 
   @Bean
---
+++ src/main/java/com/Login/Security/service/CustomUserDetailsService.java
@@ -0,0 +1,43 @@
+package com.Login.Security.service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import com.Login.Security.model.Usuario;
+import com.Login.Security.repository.UsuarioRepository;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private UsuarioRepository usuarioRepository;
+
+    public CustomUserDetailsService(UsuarioRepository usuarioRepository) {
+        this.usuarioRepository = usuarioRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<Usuario> optionalUsuario = usuarioRepository.findByEmail(username);
+        if (optionalUsuario.isPresent()) {
+            Usuario usuario = optionalUsuario.get();
+            List<GrantedAuthority> authorities = new ArrayList<>();
+            // Adicione as roles do usuário como GrantedAuthority, se necessário
+            // authorities.add(new SimpleGrantedAuthority("ROLE_USER"));
+
+            return User.withUsername(usuario.getEmail())
+                    .password(usuario.getSenha())
+                    .authorities(authorities)
+                    .build();
+        } else {
+            throw new UsernameNotFoundException("Usuário não encontrado");
+        }
+    }
+}
